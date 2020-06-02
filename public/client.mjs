a231c79f603d5d15c5922cedbb1ee9cc76a7313c
Kamen <kamen981@hotmail.com>
2020-06-02 21:51:56 +0100

    firebase initialization

--- public/client.mjs
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-//alert("hellow world!");
-//console.log("hellow world!");
-var answer = 41;
-anwser = answer + 1;
-console.log(answer);
---
+++ public/fb.js
@@ -0,0 +1,15 @@
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAfDJD1TpKyk5dJ_H8xFDqdDoBOGvfz3y4",
+    authDomain: "footballindexapp.firebaseapp.com",
+    databaseURL: "https://footballindexapp.firebaseio.com",
+    projectId: "footballindexapp",
+    storageBucket: "footballindexapp.appspot.com",
+    messagingSenderId: "873416892473",
+    appId: "1:873416892473:web:f83d98128a96eda3a3ede8",
+    measurementId: "G-J156R60Q25"
+  };
+  firebase.initializeApp(firebaseConfig);
+  
+  console.log("firebase initialized");
+}
