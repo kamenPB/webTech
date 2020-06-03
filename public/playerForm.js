65c25595eba6f5ea91b517d5f295144764f006f8
Kamen <kamen981@hotmail.com>
2020-06-03 01:34:59 +0100

    add simple data to firebase

---
+++ public/playerForm.js
@@ -0,0 +1,52 @@
+'use strict';
+
+//grab a form
+const form = document.querySelector('#addPlayer');
+
+//grab an input
+const inputName = form.querySelector('#inputName');
+const inputAge = form.querySelector('#inputAge');
+
+
+//config your firebase push
+const firebaseConfig = {
+  apiKey: "AIzaSyAfDJD1TpKyk5dJ_H8xFDqdDoBOGvfz3y4",
+  authDomain: "footballindexapp.firebaseapp.com",
+  databaseURL: "https://footballindexapp.firebaseio.com",
+  projectId: "footballindexapp",
+  storageBucket: "footballindexapp.appspot.com",
+  messagingSenderId: "873416892473",
+  appId: "1:873416892473:web:f83d98128a96eda3a3ede8",
+  measurementId: "G-J156R60Q25"
+};
+
+
+//create a functions to push
+    function firebasePush(name, age) {
+
+
+        //prevents from braking
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+
+        //push itself
+        var playersRef = firebase.database().ref('players').push().set(
+            {
+                name: name,
+                age: age
+            }
+        );
+
+    }
+
+//push on form submit
+    if (form) {
+        form.addEventListener('submit', function (evt) {
+            evt.preventDefault();
+            firebasePush(inputName.value, inputAge.value);
+
+            //shows alert if everything went well.
+            return alert('Data Successfully Sent to Realtime Database');
+        })
+    }
