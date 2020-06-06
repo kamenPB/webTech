fe3be66a0cd3c7a28c17d7ffc7250b794efaef3a
Kamen <kamen981@hotmail.com>
2020-06-06 01:56:09 +0100

    display comm and profit in portfolio table

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -6,15 +6,48 @@ dbRefPlayers.on("child_added", snap => {
   const tr = document.createElement("tr");
 
   var data = snap.val();
+
   var tdName = document.createElement("td");
   tdName.innerText = data.name;
   tr.appendChild(tdName);
+
   var tdAge = document.createElement("td");
   tdAge.innerText = data.age;
   tr.appendChild(tdAge);
+
   var tdCountry = document.createElement("td");
   tdCountry.innerText = data.country;
   tr.appendChild(tdCountry);
+
+  var tdShares = document.createElement("td");
+  tdShares.innerText = data.shares;
+  tr.appendChild(tdShares);
+
+  var tdDividends = document.createElement("td");
+  var dividends = fixed2(data.dividends);
+  tdDividends.innerText = "£" + dividends;
+  tr.appendChild(tdDividends);
+
+  // add a span element inside td + a class if I want to include css for the value and cost
+  var tdBuyPrice = document.createElement("td");
+  var cost = fixed2(data.buy_price * data.shares);
+  tdBuyPrice.innerText = "£" + data.buy_price + " (£" + cost + ")";
+  tr.appendChild(tdBuyPrice);
+
+  var tdCurrentPrice = document.createElement("td");
+  var value = fixed2(data.current_price * data.shares);
+  tdCurrentPrice.innerText = "£" + data.current_price + " (£" + value + ")";
+  tr.appendChild(tdCurrentPrice);
+
+  var tdCommission = document.createElement("td");
+  var comm = fixed2(value * 0.02);
+  tdCommission.innerText = "£" + comm;
+  tr.appendChild(tdCommission);
+
+  var tdProfit = document.createElement("td");
+  var profit = fixed2(Number(value) + Number(dividends) - Number(cost) - Number(comm));
+  tdProfit.innerText = "£" + profit;
+  tr.appendChild(tdProfit);
 
   tr.id = snap.key;
 
@@ -31,3 +64,8 @@ dbRefPlayers.on("child_removed", snap => {
   const trToRemove = document.getElementById(snap.key);
   trToRemove.parentNode.removeChild(trToRemove);
 });
+
+
+function fixed2(n){
+  return (Math.round( n * 100 + Number.EPSILON ) / 100).toFixed(2)
+}
